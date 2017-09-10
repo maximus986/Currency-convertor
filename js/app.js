f0d7e08f672f46e0ea4fa5d796bd570e29a29a6c
Aleksandar Maksimovic <aleksandar86va@gmail.com>
2017-09-10 15:53:36 +0200

    Code refactor, calculate currencies and do responsive

--- js/app.js
+++ js/app.js
@@ -2,142 +2,110 @@
 
     var widgetFrom = document.querySelector(".widget-from");
     var widgetTo = document.querySelector(".widget-to");
+    var selectFrom = document.getElementById("currency-from");
+    var selectTo = document.getElementById("currency-to");
+    var ammountFrom = document.getElementById("ammount-from");
+    var ammountTo = document.getElementById("ammount-to");
+    var reset = document.getElementById("reset-btn");
+    var errorMessage = widgetFrom.querySelector("p");
+    var completeServerData;
+    var serverData = {};
+
 
     makeRequest();
 
-    function initWidgets(data) {
+    selectFrom.onchange = handleInputChange;
+    selectTo.onchange = handleInputChange;
+    ammountFrom.addEventListener("keyup", function() {
+        handleInputChange();
+    });
+    reset.addEventListener("click", setInitialState);
 
-        var selectFrom = document.createElement("select");
-        selectFrom.id = "slct1";
-        selectFrom.setAttribute("name", "slct1");
-
-        // Option Eur
-        var optionEur = document.createElement("option");
-        optionEur.setAttribute("value", "eur");
-        optionEur.setAttribute("selected", true);
-        var optionEurText = document.createTextNode("EUR");
-        optionEur.appendChild(optionEurText);
-
-        //Option RSD
-        var optionRsd = document.createElement("option");
-        optionRsd.setAttribute("value", "rsd");
-        var optionRsdText = document.createTextNode("RSD");
-        optionRsd.appendChild(optionRsdText);
-
-        //Option USD
-        var optionUsd = document.createElement("option");
-        optionUsd.setAttribute("value", "usd");
-        var optionUsdText = document.createTextNode("USD");
-        optionUsd.appendChild(optionUsdText);
-
-        selectFrom.append(optionEur, optionRsd, optionUsd);
-        widgetFrom.appendChild(selectFrom);
-
-        //input
-        var valueToConvert = document.createElement("input");
-        valueToConvert.id = "value-to-convert";
-        valueToConvert.setAttribute("type", "number");
-        widgetFrom.appendChild(valueToConvert);
-
-        //Widget rsd to convert to
-        var selectTo = document.createElement("select");
-        selectTo.id = "slct2";
-        selectTo.setAttribute("name", "slct2");
-
-        //Option RSD
-        var optionRsd = document.createElement("option");
-        optionRsd.setAttribute("value", "rsd");
-        optionRsd.setAttribute("selected", true);
-        var optionRsdText = document.createTextNode("RSD");
-        optionRsd.appendChild(optionRsdText);
-
-        // Option Eur
-        var optionEur = document.createElement("option");
-        optionEur.setAttribute("value", "eur");
-        var optionEurText = document.createTextNode("EUR");
-        optionEur.appendChild(optionEurText);
-
-        //Option USD
-        var optionUsd = document.createElement("option");
-        optionUsd.setAttribute("value", "usd");
-        var optionUsdText = document.createTextNode("USD");
-        optionUsd.appendChild(optionUsdText);
-
-        selectTo.append(optionRsd, optionEur, optionUsd);
-        widgetTo.appendChild(selectTo);
-
-        var convertedValue = document.createElement("input");
-        convertedValue.id = "converted-value";
-        convertedValue.setAttribute("type", "number");
-        widgetTo.appendChild(convertedValue);
-
-        // Initial value of both widgets
-        valueToConvert.value = 1;
-        convertedValue.value = parseFloat(data.eur.pro).toFixed(2);
-
-        slct1.querySelector('option[value="rsd"]').classList.add("deleted");
-        slct2.querySelector('option[value="eur"]').classList.add("deleted");
-
-
-        document.addEventListener("DOMContentLoaded", function() {
-            slct1.onchange = currencyToConvert;
-        }, false);
-
-        function currencyToConvert(event) {
-            if (event.target.value === "rsd") {
-                updateValue(convertedValue, "rsd", "eur", parseFloat(1 / data.eur.pro).toFixed(2));
-                updateValue(convertedValue, "eur", "rsd", parseFloat(data.eur.pro).toFixed(2));
-                updateValue(convertedValue, "rsd", "usd", parseFloat(1 / data.usd.pro).toFixed(2));
-                slct2.querySelector('option[value="rsd"]').classList.add("deleted");
-                slct2.querySelector('option[value="eur"]').classList.remove("deleted");
-            } else if (event.target.value === "usd") {
-                updateValue(convertedValue, "usd", "eur", parseFloat(data.usd.pro / data.eur.pro).toFixed(2));
-                updateValue(convertedValue, "eur", "usd", parseFloat(data.eur.pro / data.usd.pro).toFixed(2));
-                updateValue(convertedValue, "usd", "rsd", parseFloat(data.usd.pro).toFixed(2));
-                slct2.querySelector('option[value="usd"]').classList.add("deleted");
-                slct2.querySelector('option[value="rsd"]').classList.remove("deleted");
-                slct2.querySelector('option[value="eur"]').classList.remove("deleted");
-            } else if (event.target.value === "eur") {
-                updateValue(convertedValue, "rsd", "eur", parseFloat(1 / data.eur.pro).toFixed(2));
-                updateValue(convertedValue, "eur", "rsd", parseFloat(data.eur.pro).toFixed(2));
-                updateValue(convertedValue, "eur", "usd", parseFloat(data.eur.pro / data.usd.pro).toFixed(2));
-                slct2.querySelector('option[value="eur"]').classList.add("deleted");
-                slct2.querySelector('option[value="usd"]').classList.remove("deleted");
-                slct2.querySelector('option[value="rsd"]').classList.remove("deleted");
-            }
+    function handleInputChange() {
+        var currencyFrom = selectFrom.options[selectFrom.selectedIndex].value;
+        var currencyTo = selectTo.options[selectTo.selectedIndex].value;
+        var ammount = ammountFrom.value;
+        if (!ammount) {
+            ammount = 1;
         }
 
-        document.addEventListener("DOMContentLoaded", function() {
-            slct2.onchange = updateCurrency;
-        }, false);
-
-        function updateCurrency(event) {
-            if (event.target.value === "usd") {
-                updateValue(convertedValue, "eur", "usd", parseFloat(data.eur.pro / data.usd.pro).toFixed(2));
-                updateValue(convertedValue, "rsd", "usd", parseFloat(data.usd.pro).toFixed(2));
-                slct1.querySelector('option[value="usd"]').classList.add("deleted");
-                slct1.querySelector('option[value="rsd"]').classList.remove("deleted");
-                slct1.querySelector('option[value="eur"]').classList.remove("deleted");
-            } else if (event.target.value === "rsd") {
-                updateValue(convertedValue, "eur", "rsd", parseFloat(data.eur.pro).toFixed(2));
-                updateValue(convertedValue, "usd", "rsd", parseFloat(data.usd.pro).toFixed(2));
-                slct1.querySelector('option[value="rsd"]').classList.add("deleted");
-                slct1.querySelector('option[value="eur"]').classList.remove("deleted");
-                slct1.querySelector('option[value="usd"]').classList.remove("deleted");
-            } else if (event.target.value === "eur") {
-                updateValue(convertedValue, "rsd", "eur", parseFloat(1 / data.eur.pro).toFixed(2));
-                updateValue(convertedValue, "usd", "eur", parseFloat(data.usd.pro / data.eur.pro).toFixed(2));
-                slct1.querySelector('option[value="eur"]').classList.add("deleted");
-                slct1.querySelector('option[value="usd"]').classList.remove("deleted");
-                slct1.querySelector('option[value="rsd"]').classList.remove("deleted");
-            }
+        if ((parseInt(ammount) + '') !== ammount && ammount.length > 0) {
+            ammount = 1;
+            errorMessage.classList.remove("deleted");
+            ammountFrom.classList.add("warning");
+        } else {
+            errorMessage.classList.add("deleted");
+            ammountFrom.classList.remove("warning");
         }
+
+        ammountTo.value = convertCurrency(currencyFrom, currencyTo, ammount);
+
+        excludeCurrencies(currencyFrom, currencyTo);
     }
 
-    function updateValue(convertedValue, currencyFrom, currencyTo, currencyValue) {
-        if (slct1.value === currencyFrom && slct2.value === currencyTo) {
-            convertedValue.value = currencyValue;
+    function convertCurrency(currencyFrom, currencyTo, ammount) {
+        if (currencyTo === "rsd") {
+            var convertedCurrency = serverData[currencyFrom].sre * ammount;
+
+            return convertedCurrency.toFixed(2);
         }
+
+        if (currencyFrom === "rsd") {
+            var convertedCurrency = ammount * (1 / serverData[currencyTo].sre);
+
+            return convertedCurrency.toFixed(2);
+        }
+
+        var convertedCurrency = ammount * (serverData[currencyFrom].sre / serverData[currencyTo].sre);
+
+        return convertedCurrency.toFixed(2);
+    }
+
+    function setInitialState() {
+        ammountFrom.value = 1;
+        selectFrom.selectedIndex = 2;
+        selectTo.selectedIndex = 0;
+        handleInputChange();
+    }
+
+    function initWidgets() {
+        serverData.rsd = {};
+        serverData.usd = completeServerData["usd"];
+        serverData.eur = completeServerData["eur"];
+
+        for (var currency in serverData) {
+            if (serverData.hasOwnProperty(currency)) {
+                selectFrom.appendChild(createOption(currency));
+                selectTo.appendChild(createOption(currency));
+            }
+        }
+
+        function createOption(currency) {
+            var option = document.createElement("option");
+            var optionText = document.createTextNode(currency);
+            option.setAttribute("value", currency);
+            option.appendChild(optionText);
+
+            return option;
+        }
+
+        setInitialState();
+    }
+
+    function excludeCurrencies(currencyFrom, currencyTo) {
+        var selectToDeleted = selectTo.querySelectorAll(".deleted");
+        for (var i = 0; i < selectToDeleted.length; i++) {
+            selectToDeleted[i].classList.remove("deleted");
+        }
+
+        selectTo.querySelector('option[value="' + currencyFrom + '"]').classList.add("deleted");
+
+        var selectFromDeleted = selectFrom.querySelectorAll(".deleted");
+        for (var i = 0; i < selectFromDeleted.length; i++) {
+            selectFromDeleted[i].classList.remove("deleted");
+        }
+
+        selectFrom.querySelector('option[value="' + currencyTo + '"]').classList.add("deleted");
     }
 
     function makeRequest() {
@@ -147,16 +115,16 @@
             alert('Giving up :( Cannot create an XMLHTTP instance');
             return false;
         }
-        httpRequest.onreadystatechange = alertContents;
+        httpRequest.onreadystatechange = parseData;
         httpRequest.open('GET', '/kursna-lista');
         httpRequest.send();
     }
 
-    function alertContents() {
+    function parseData() {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
             if (httpRequest.status === 200) {
-                var data = JSON.parse(httpRequest.responseText).result;
-                initWidgets(data);
+                completeServerData = JSON.parse(httpRequest.responseText).result;
+                initWidgets();
             } else {
                 alert('Something went wrong');
             }
